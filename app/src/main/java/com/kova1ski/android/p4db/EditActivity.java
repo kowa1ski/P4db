04f5e9e8429594d4053e639e8fea88fc78914b76
kowa1ski <kowalski@gmx.es>
2017-09-11 17:43:34 +0200

    EDITION creada e INTENTada. CyF

---
+++ app/src/main/java/com/kova1ski/android/p4db/EditActivity.java
@@ -0,0 +1,13 @@
+package com.kova1ski.android.p4db;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+    }
+}
