d5753a8535154ad4fa6d7e42059cf5b86c0f21a0
kowa1ski <kowalski@gmx.es>
2017-09-12 10:40:16 +0200

    MENU MAIN -. Implantado botón molón.

--- app/src/main/java/com/kova1ski/android/p4db/MainActivity.java
+++ app/src/main/java/com/kova1ski/android/p4db/MainActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,14 +26,13 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, EditActivity.class);
                 startActivity(intent);
-                //region Description  -- Esto que hay aquí dentro ya no va a servir
-                // Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                   //     .setAction("Action", null).show();
-                //endregion
             }
         });
     }
 
+    // NOTA IMPORTANTE ---- Sobre el inflate del MENU. este método es únicamente
+    // para poder inflar el MENU. Para obtener las respuestas a los items seleccionados
+    // está el siguiente que es onOptionsItemSelected, con una variable item
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -40,6 +40,9 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    // Este es el método que tenemos que usar para darle funcionalidad a los
+    // diferentes botones que metamos en el menú.
+    // En concreto, este método se ha generado automáticamente al crear esta app.
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -47,8 +50,14 @@ public class MainActivity extends AppCompatActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        //noinspection SimplifiableIfStatement  --- (Esto lo ponía y no sé qué significa)
+        // Vamos a cambiar este if por un switch. En esta app no hace falta pero quiero que
+        // sea siempre así para estar siempre preparado para que en el caso de que quiera
+        // añadir más botones, tener el código preparado. Aunque si quiero añadir más botones
+        // bastaría con anidar unos , if , dentro de otros sin problema. Prefiero el switch.
+        switch (id){
+            case R.id.action_main_molon:
+                Toast.makeText(this, "Has pulsado el botón molón", Toast.LENGTH_SHORT).show();
             return true;
         }
 
