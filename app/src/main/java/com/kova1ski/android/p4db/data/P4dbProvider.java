06845f9f0f9be65b852a956dad94532f40a4c152
kowa1ski <kowalski@gmx.es>
2017-09-13 18:15:43 +0200

    PROVIDER -. URIMATCHER nombrado.

--- app/src/main/java/com/kova1ski/android/p4db/data/P4dbProvider.java
+++ app/src/main/java/com/kova1ski/android/p4db/data/P4dbProvider.java
@@ -2,6 +2,7 @@ package com.kova1ski.android.p4db.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
@@ -12,6 +13,20 @@ import android.support.annotation.Nullable;
  */
 
 public class P4dbProvider extends ContentProvider {
+
+    // Declaramos el código URIMATCHER para el contenido del uri en ambos casos.
+    private static final int TODA_LA_TABLA = 100 ;
+    private static final int SINGLE_ITEM_ID = 101 ;
+
+    // Creamos el URIMATCHER
+    private static final UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH) ;
+
+    // Y dejamos hechas las dos direcciones de acceso
+    static {
+        uriMatcher.addURI(P4dbContract.CONTENT_AUTHORITY, P4dbContract.PATH_SEGMENT, TODA_LA_TABLA);
+        uriMatcher.addURI(P4dbContract.CONTENT_AUTHORITY, P4dbContract.PATH_SEGMENT, SINGLE_ITEM_ID);
+    }
+
     @Override
     public boolean onCreate() {
         return false;
