f28f53b6e3a7d4ff5fb00eeb3b636118e18d1a9b
kowa1ski <kowalski@gmx.es>
2017-09-21 13:17:33 +0200

    EDITAR - editText campos ya se rellenan. Mi método del THIS es una pasada. Implementa todo el loader necesario de golpe. Es fácil y estoy feliz.

--- app/src/main/java/com/kova1ski/android/p4db/EditActivity.java
+++ app/src/main/java/com/kova1ski/android/p4db/EditActivity.java
@@ -1,7 +1,11 @@
 package com.kova1ski.android.p4db;
 
+import android.app.LoaderManager;
 import android.content.ContentValues;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -14,7 +18,10 @@ import android.widget.Toast;
 import com.kova1ski.android.p4db.data.P4dbContract;
 import com.kova1ski.android.p4db.data.P4dbHelper;
 
-public class EditActivity extends AppCompatActivity {
+// Esta línea ha cambiado pero no se ha mecanizado a mano,
+// viene DISPARADA WEYYY desde el THIS del initLoader.
+// Recordemos, eso sí, SUSTITUIR el Object por Cursor.
+public class EditActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
     // Declaramos los editText
     // IMPORTANTE AQUÍ SÓLO SE DECLARAN, LUEGO SE
@@ -29,6 +36,10 @@ public class EditActivity extends AppCompatActivity {
     // Declaramos aquí el currentItemUri con el mismo nombre que en la otra
     // actividad porque LO VAMOS A RESCATAR. yeeeaaaaa!!!!
     private Uri currentItemUri;
+
+    // Vamos con vista a crear un loader que nos carque los editText
+    // Aquí hay que inicializar su inicio a 0.
+    private static final int EXISTING_ITEM_LOADER = 0 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +65,16 @@ public class EditActivity extends AppCompatActivity {
             // Este es el caso en que NO ES null, sino que viene cargadito :-)
             // Vamos a decirle que cambie el título.
             setTitle("EDICIÓN DE ITEM");
+
+            // getLOADERmanager.
+            // Vamos a darle caña a este , else ,.
+            // Desde aquí vamos, directamente a inicializar el loader y
+            // así, aprovechamos su THIS para disparar la secuencia del
+            // implement y los métodos.
+            getLoaderManager().initLoader(EXISTING_ITEM_LOADER, null, this);
+            // Para que nos vamos a mentir, este THIS es una maravilla
+            // Eso sí debemos recordar cambiar los Object por defecto por Cursor.
+
         }
 
 
@@ -151,4 +172,78 @@ public class EditActivity extends AppCompatActivity {
                     "AÑADIDO CORRECTAMENTE", Toast.LENGTH_SHORT).show();
         }
     }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+
+        // Aquí nada nuevo. Vamos a cargar un cursorLoader que
+        // vamos a retornar. O mejor dicho vamos a RETORNAR UN CursorLoader.
+        // Lo de siempre, projection y adelante.
+        String[] projection = {
+                P4dbContract.P4dbEntry._ID,
+                P4dbContract.P4dbEntry.CN_NOMBRE,
+                P4dbContract.P4dbEntry.CN_PESO };
+
+        return new CursorLoader(this,
+                currentItemUri,
+                projection,
+                null,
+                null,
+                null);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
+
+        // Una vez que ya ha finalizado la primera parte del loader en segundo
+        // plano, tenemos que coger ese CursorLoader cargadito.
+        // Antes de nada nos tenemos que asegurar que está cargadito. Si
+        // tiene menos de una fila es que no hay nada que aprovechar y también
+        // que algo no ha ido bien.
+        if (cursor == null || cursor.getCount() < 1 ){
+            // En este caso no hay nada, está vacío o no tiene nada.
+            // Se retorna y ya.
+            return;
+        }
+
+        // Fuera del anterior IF, es que tod ha ido estupenamente.
+        // Vamos a leer ese CursorLoader, vamos a ver qué hay escrito en
+        // las celdas y lo vamos a copiar en los editText.
+        // Para eso vamos a tener un método. Vamos a saber numéricamente
+        // las columnas que nos interesan, luego, como ya las tenemos localizadas
+        // esas celdas sólo extraemos su información y luego la copiamos en los
+        // editText.
+        // Hacemos un IF con un MOVETOFIRST y toda la secuencia se viene sola.
+        if (cursor.moveToFirst()){
+
+            // identificamos las columnas con INT porque es la forma
+            // en que vamos a localizar las celdas.
+            int nombreColumIndex = cursor.getColumnIndex(P4dbContract.P4dbEntry.CN_NOMBRE);
+            int pesoColumIndex = cursor.getColumnIndex(P4dbContract.P4dbEntry.CN_PESO);
+
+            String nombre = cursor.getString(nombreColumIndex);
+            int peso = cursor.getInt(pesoColumIndex);
+
+            editTextNombre.setText(nombre);
+            editTextPeso.setText(Integer.toString(peso));
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+        // Y el reset es poner los editText en blanco Y YA.
+        editTextNombre.setText("");
+        editTextPeso.setText("");
+    }
 }
+
+
+
+
+
+
+
+
+
+
